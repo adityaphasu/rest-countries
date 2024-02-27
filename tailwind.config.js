8082c40d172f8595c99ff5fb8cda072cb863f786
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-27 20:56:25 +0530

    Add new colors to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very-light-gray": "hsl(0, 0%, 98%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "very-dark-blue1": "hsl(200, 15%, 8%)",
+        "very-dark-blue2": "(207, 26%, 17%)",
+        "dakr-blue": "hsl(209, 23%, 22%)",
+      },
+    },
   },
   plugins: [],
 };
