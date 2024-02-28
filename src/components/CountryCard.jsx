440f75838a7eae7e06181364a96e9b86203a2b10
Aditya Phasu <adityaphasu919@gmail.com>
2024-02-28 22:02:06 +0530

    Add CountryCard component

---
+++ src/components/CountryCard.jsx
@@ -0,0 +1,31 @@
+const CountryCard = ({ country }) => {
+  return (
+    <div className="h-full overflow-hidden rounded-lg bg-white shadow-sm transition-shadow hover:shadow-lg">
+      <img
+        src={country.flags.png}
+        alt={country.flags.alt}
+        className="w-full sm:h-44"
+      />
+      <div className="space-y-2 px-7 py-8">
+        <h2 className="text-3xl font-bold md:text-2xl">
+          {country.name.common}
+        </h2>
+        <ul>
+          <li>
+            <span className="font-semibold">Population: </span>
+            {country.population.toLocaleString()}
+          </li>
+          <li>
+            <span className="font-semibold">Region: </span>
+            {country.region}
+          </li>
+          <li>
+            <span className="font-semibold">Capital: </span>
+            {country.capital && country.capital[0]}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default CountryCard;
