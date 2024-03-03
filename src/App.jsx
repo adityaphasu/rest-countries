7db9687dc522a072da9b649d9ae0e0d1a76f44a8
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-03 20:27:14 +0530

    Add dark mode functionality to the app

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Country from "./pages/Country";
 import Home from "./pages/Home";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+    setIsDark(storedDarkMode === "true");
+  }, []);
+
   return (
-    <>
+    <div
+      className={`${isDark ? "dark" : ""} h-full dark:bg-very-dark-blue2 dark:text-white`}
+    >
       <header>
-        <Navbar />
+        <Navbar isDark={isDark} setIsDark={setIsDark} />
       </header>
-      <main>
+      <main className="dark:bg-very-dark-blue2">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/country/:countryName" element={<Country />} />
         </Routes>
       </main>
-    </>
+    </div>
   );
 };
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import { Moon } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
+import { useEffect } from "react";
 
-const Navbar = () => {
+const Navbar = ({ isDark, setIsDark }) => {
+  const toggleDarkMode = () => {
+    setIsDark((prev) => !prev);
+    localStorage.setItem("darkMode", !isDark);
+  };
+
   return (
-    <nav className="flex items-center justify-between bg-white p-5 shadow-sm md:px-12">
-      <h1 className="font-bold md:text-2xl">Where in the world?</h1>
+    <nav className="flex items-center justify-between bg-white p-5 shadow-sm md:px-12 dark:bg-dark-blue dark:shadow-very-dark-blue2">
+      <h1 className="text-xl font-bold sm:text-2xl">Where in the world?</h1>
 
-      <button className="flex items-center gap-2 rounded-md border border-black p-2 font-medium">
-        <Moon />
-        Dark Mode
+      <button
+        className="flex items-center justify-center gap-2 rounded-md p-2 font-medium transition-colors duration-300 ease-in-out hover:bg-dark-blue hover:text-white sm:w-[8.2rem] dark:hover:bg-white dark:hover:text-dark-blue"
+        onClick={toggleDarkMode}
+      >
+        {isDark ? <Sun /> : <Moon />}
+        <span className="hidden sm:block">
+          {isDark ? "Light" : "Dark"} Mode
+        </span>
       </button>
     </nav>
   );
