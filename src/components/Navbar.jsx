61c0b6fb34612cdf39175224cf466f884141c210
Aditya Phasu <adityaphasu919@gmail.com>
2024-03-05 18:59:24 +0530

    Update Navbar heading and add accessibility attributes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,13 +9,15 @@ const Navbar = ({ isDark, setIsDark }) => {
 
   return (
     <nav className="flex items-center justify-between bg-white p-5 shadow-sm md:px-12 dark:bg-dark-blue dark:shadow-very-dark-blue2">
-      <h1 className="text-xl font-bold sm:text-2xl">Where in the world?</h1>
+      <h2 className="text-xl font-bold sm:text-2xl">Where in the world?</h2>
 
       <button
         className="flex items-center justify-center gap-2 rounded-md p-2 font-medium transition-colors duration-300 ease-in-out hover:bg-dark-blue hover:text-white sm:w-[8.2rem] dark:hover:bg-white dark:hover:text-dark-blue"
         onClick={toggleDarkMode}
+        aria-label={`Switch to ${isDark ? "Light" : "Dark"} Mode`}
+        aria-pressed={isDark}
       >
-        {isDark ? <Sun aria-label="Sun" /> : <Moon aria-label="Moon" />}
+        {isDark ? <Sun aria-hidden="true" /> : <Moon aria-hidden="true" />}
         <span className="hidden sm:block">
           {isDark ? "Light" : "Dark"} Mode
         </span>
